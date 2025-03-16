f9922f475726fe6a6032869be3ee1669beb986ee
Try43-loki <trykhomrean43@gmail.com>
2025-03-16 14:10:49 +0700

    move

---
+++ src/data/cardCategory.js
@@ -0,0 +1,21 @@
+const cardsList = [
+  {
+    id: 1,
+    icon: "Tags",
+    tag: "Books",
+    path: "/books",
+    image:
+      "https://s3-alpha-sig.figma.com/img/c73c/a81c/dd317b2dcd79328efc4e3095489f2244?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=sop1LVQptT1zML-GVA9Q0ZDt5qkU4Apx6H9tTIZMvD9WfErQZZlubZI2SZBsPUiV-QgL5JR9VsAS6JgrjHBXt1SESaIVXrBFmThQkQwLVyxHmduniVAefQv6SVsNWMGnc-B7H6NYa0Xfe2UXRKPtt4YiDOZ1A~yE0TJUU9eOH2LUI9Sv0eygEjNy5BMuztUfPGXrGILhJOvh0fDcCI9Dlal0Bu8f7qWrZEA3WZ2~w2-Z3ben~QQRt-vz9gYO0ZsA8F4rJGDlBlDY8M2oUopxsCUND9VVyVF8VNeMcgViCSP94gidrM8hqGX60XmHafNwPvrogafTyPhZGmRI24LxZg__",
+    text: "View All Available Books",
+  },
+  {
+    id: 2,
+    icon: "Tags",
+    tag: "Cartoon",
+    path: "/cartoon",
+    image:
+      "https://s3-alpha-sig.figma.com/img/65a1/7fc0/0c5a744cd7491aa1555996062a2e9f16?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=hb9tx4C2PrUqZCAkl-zxHUI5g2SKf7f6EWq-Y3GQnDjH2h2khBxSi00YxHQggSEZUaYL5DnkyZuEVxO4RLtKmTrl9v3tYfrW4r5mUDwt4ymVp0TStjoXCc4i0xkW2YJvimF02GnwCg0PjqV7xpcCk8T0uDFPl4YpPtQndpKxGintdpXnOjp2YIYlQDx7m2HNpHlc4CfGICTlcLS5k5XK0E-RPnHPTjcHl7FpDNGHF8XUdbzph0FR4T6D8LzWiJTK6uTO3Q3nPJqrAH4Noq3c40a~cHpzb40py0-5tODiE0xsLoa9mpnq0uCUJaL~uqh3koddEdUb28abaZuzDq4s2A__",
+    text: "View All Available Cartoons",
+  },
+];
+export default cardsList;
